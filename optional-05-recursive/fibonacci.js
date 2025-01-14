176fd7994710ac6bfd5bc3017f636b59e4b65563
Izzuddin Ahmad Afif <izzuddinafif@gmail.com>
2025-01-14 16:30:26 +0700

    solve 05-recursive

--- optional-05-recursive/fibonacci.js
+++ optional-05-recursive/fibonacci.js
@@ -1,6 +1,16 @@
 function fibonacci(n) {
-
+    if (n == 1) {
+        return [0, 1]
+    } else if (n == 0){
+        return [0]
+    } else {
+        let arr = [...fibonacci(n-1)]
+        // console.log(arr.at(-1), n)
+        return [...arr, arr.at(-1) + arr.at(-2)]
+    }
 }
 
+
+// console.log(fibonacci(10))
 // Jangan hapus kode di bawah ini!
 export default fibonacci;
